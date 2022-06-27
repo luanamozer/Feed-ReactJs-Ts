62cee2931d4e237ed92877c501d84b1b792b1c64
Luana Mozer <luanamozzer1996@gmail.com>
2022-06-27 12:14:39 -0300

    adicionando Typescript na aplicação

---
+++ src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import styles from "./Comment.module.css";
+import { Trash, ThumbsUp } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time
+                title="20 de Junho,2022 ás 11:11h"
+                dateTime="2022-06-20 ás 11:11:33"
+              >
+                Cerca de 2hr atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
